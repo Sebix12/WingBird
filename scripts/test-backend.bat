a962a82af4dec0e037abde13db797263ad3e32da
cablesalty <cablesalty@gmail.com>
2024-06-03 15:12:04 +0200

    Update test-backend.bat

--- scripts/test-backend.bat
+++ scripts/test-backend.bat
@@ -2,3 +2,9 @@
 set get=%*
 
 echo Hello!
+echo This script can be run in a source file
+echo using "runscript". The difference between
+echo script files and components are that
+echo components are run when they are compiled.
+echo However, script files are run when a user
+echo visits the site (just like PHP)!
