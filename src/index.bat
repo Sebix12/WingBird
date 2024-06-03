3f8d25f9c0832c7921f7fa612180348ba1df9d2a
cablesalty <cablesalty@gmail.com>
2024-06-03 15:56:48 +0200

    Update example

--- src/index.bat
+++ src/index.bat
@@ -3,6 +3,9 @@ set component=%cd%\components
 set script=%cd%\scripts
 cd commands\html
 
+call doctypehtml
+
+
 :: Create paragraph
 call paragraph Hello World!
 
@@ -11,6 +14,9 @@ call %component%\google-link
 
 :: Run test backend script file
 call runscript %script%\test-backend.bat
+
+
+call endhtml
 
 :EOF
 call ..\endfile index.php
