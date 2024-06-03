7513cf00254be05eee37965adc76344f2cbaa024
cablesalty <cablesalty@gmail.com>
2024-06-03 15:51:43 +0200

    Update script example

--- scripts/test-backend.bat
+++ scripts/test-backend.bat
@@ -1,10 +1,7 @@
 @echo off
 set get=%*
 
-echo Hello!
-echo This script can be run in a source file
-echo using "runscript". The difference between
-echo script files and components are that
-echo components are run when they are compiled.
-echo However, script files are run when a user
-echo visits the site (just like PHP)!
+echo This script is run every time someone visits the site!
+echo.
+set /a twoplustwo=2+2
+echo The result of 2 + 2 is %twoplustwo%
